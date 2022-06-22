b676647356cd2649229875181af02e954b2d1b89
Mohammad Hossein <hsn.gerami@gmail.com>
2022-06-22 18:27:09 +0430

    append controller and services by deocrators to have more readable routers

---
+++ configurations/dependency_injection.py
@@ -0,0 +1,31 @@
+
+
+# def inject(target, ControllerClass):
+#
+#     def decorator_init(self):
+#         print("Decorator running")
+#         self.controller = ControllerClass()
+#
+#     target.__init__ = decorator_init
+#     return target
+import inspect
+
+
+def inject(**kwargs):
+    def decorator(cls):
+        for key in kwargs:
+            def closure(key, attr):
+                def getAttr(self):
+                    value = kwargs[key]
+                    return value() if inspect.isclass(value) else value
+
+                def setAttr(self, value):
+                    setattr(self, key, value() if inspect.isclass(value) else value)
+
+                prop = property(getAttr, setAttr)
+                setattr(cls, key, prop)
+
+            closure(key, kwargs[key])
+        return cls
+
+    return decorator
